fe4076a561d39c2190af3ccc44ce72a65378c6eb
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-03-22 12:41:49 +0100

    feat: add projects in development category to full-projects

--- components/projects-full/projects-full.tsx
+++ components/projects-full/projects-full.tsx
@@ -1,5 +1,5 @@
 import { ProjectFullCard } from "./project-full-card";
-
+import { Separator } from "../ui/separator";
 const projects = [
   {
     title: "Project 1",
@@ -9,6 +9,7 @@ const projects = [
     github: "https://github.com/project1",
     link: "https://ludvigbergstrom.com",
     image: null,
+    dev: false,
   },
   {
     title: "Project 2",
@@ -19,6 +20,7 @@ const projects = [
     github: "https://github.com/project2",
     link: "https://project2.com",
     image: null,
+    dev: false,
   },
   {
     title: "Project 3",
@@ -29,6 +31,7 @@ const projects = [
     github: "https://github.com/project3",
     link: "https://project3.com",
     image: null,
+    dev: false,
   },
   {
     title: "Project 4",
@@ -39,6 +42,7 @@ const projects = [
     github: "https://github.com/project4",
     link: "https://project4.com",
     image: null,
+    dev: false,
   },
   {
     title: "Project 5",
@@ -49,6 +53,7 @@ const projects = [
     github: "https://github.com/project5",
     link: "https://project5.com",
     image: null,
+    dev: true,
   },
   {
     title: "Project 6",
@@ -58,22 +63,32 @@ const projects = [
     github: "https://github.com/project6",
     link: "https://project6.com",
     image: null,
+    dev: true,
   },
 ];
 
 export type Project = (typeof projects)[number];
-
+const firstDevProjectIndex = projects.findIndex((project) => project.dev);
 export function ProjectsFull() {
   return (
     <section className='main-container py-20'>
       <div className='flex flex-col  relative'>
         {projects.map((project, index) => (
-          <ProjectFullCard
-            key={project.title}
-            project={project}
-            reverse={index % 2 !== 0}
-            index={index}
-          />
+          <>
+            <ProjectFullCard
+              key={project.title}
+              project={project}
+              reverse={index % 2 !== 0}
+              index={index}
+            />
+            {index === firstDevProjectIndex && (
+              <Separator position='left'>
+                <h2 className='text-2xl md:text-4xl font-bold mb-0 text-center'>
+                  Projects in development
+                </h2>
+              </Separator>
+            )}
+          </>
         ))}
       </div>
     </section>
--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,12 +1,38 @@
-export function Separator({ children }: { children?: React.ReactNode }) {
+import { cn } from "@/utils/cn"; // Assuming you have a cn utility
+
+interface SeparatorProps {
+  children?: React.ReactNode;
+  className?: string;
+  position?: "left" | "center" | "right";
+}
+
+export function Separator({
+  children,
+  className,
+  position = "center",
+}: SeparatorProps) {
   return (
-    <div className='relative w-full flex items-center justify-center my-8 m'>
+    <div
+      className={cn(
+        "relative w-full flex items-center justify-center my-8",
+        className
+      )}
+    >
       {children ? (
         <>
           <div className='absolute inset-0 flex items-center'>
             <div className='w-full h-px bg-background-tertiary'></div>
           </div>
-          <div className='relative z-10 px-6 bg-background-primary text-center'>
+          <div
+            className={cn(
+              "relative z-10 bg-background-primary",
+              position === "left"
+                ? "text-left ml-0 mr-auto pr-6"
+                : position === "right"
+                ? "text-right ml-auto mr-0 pl-6"
+                : "text-center mx-auto px-6"
+            )}
+          >
             {children}
           </div>
         </>
